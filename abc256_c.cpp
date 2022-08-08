8d00df08976dc9ec87429918c500b2d5f752afc4
sahokk <lovedrama0428@gmail.com>
2022-08-08 16:08:30 +0900

    全然時間内に終わる

---
+++ abc256_c.cpp
@@ -0,0 +1,96 @@
+//include
+//------------------------------------------
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+//conversion
+//------------------------------------------
+inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
+template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return sout.str();}
+
+//math
+//-------------------------------------------
+template<class T> inline T sqr(T x) {return x*x;}
+
+//typedef
+//------------------------------------------
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef long long ll;
+
+//container util
+//------------------------------------------
+#define ALL(a)  (a).begin(),(a).end()
+#define RALL(a) (a).rbegin(), (a).rend()
+#define PB push_back
+#define MP make_pair
+#define SZ(a) int((a).size())
+#define EACH(i,c) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); ++i)
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+#define SORT(c) sort((c).begin(),(c).end())
+
+//repetition
+//------------------------------------------
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+//constant
+//--------------------------------------------
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+
+//clear memory
+#define CLR(a) memset((a), 0 ,sizeof(a))
+
+//debug
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+
+
+int main(){
+    VI H(3),W(3);
+    REP(i,3) cin >> H[i];
+    REP(i,3) cin >> W[i];
+    
+    int ans = 0;
+
+    for(int a = 1; a<=30; a++){
+        for(int b = 1; b<=30; b++){
+            for(int d = 1; d<=30; d++){
+                for(int e = 1; e<=30; e++){
+                    int c = H[0] - a - b;
+                    int f = H[1] - d - e;
+                    int g = W[0] - a - d;
+                    int h = W[1] - b - e;
+                    int i = W[2] - c - f;
+                    if(min({c,f,g,h,i})>0 && g+h+i == H[2]) ans++;
+                }
+            }
+        }
+    }
+
+    cout << ans << endl;
+}
